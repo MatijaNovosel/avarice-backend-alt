84b392e36b39ce1aece848e8ce4e1e1c18b2aee7
Matija Novosel <mnovosel5@gmail.com>
2023-11-16 13:11:13 +0100

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,43 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { Logger, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { PrismaModule, loggingMiddleware } from "nestjs-prisma";
+import { AppController } from "./app.controller";
+import { AppResolver } from "./app.resolver";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import config from "./common/configs/config";
+import { GqlConfigService } from "./gql-config.service";
+import { PostsModule } from "./posts/posts.module";
+import { UsersModule } from "./users/users.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          // configure your prisma middleware
+          loggingMiddleware({
+            logger: new Logger("PrismaMiddleware"),
+            logLevel: "log"
+          })
+        ]
+      }
+    }),
+
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService
+    }),
+
+    AuthModule,
+    UsersModule,
+    PostsModule
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver]
+})
+export class AppModule {}
---
+++ src/users/models/user.model.ts
@@ -0,0 +1,38 @@
+import {
+  Field,
+  HideField,
+  ObjectType,
+  registerEnumType
+} from "@nestjs/graphql";
+import { Role } from "@prisma/client";
+import { IsEmail } from "class-validator";
+import "reflect-metadata";
+import { BaseModel } from "../../common/models/base.model";
+import { Post } from "../../posts/models/post.model";
+
+registerEnumType(Role, {
+  name: "Role",
+  description: "User role"
+});
+
+@ObjectType()
+export class User extends BaseModel {
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field(() => String, { nullable: true })
+  firstname?: string;
+
+  @Field(() => String, { nullable: true })
+  lastname?: string;
+
+  @Field(() => Role)
+  role: Role;
+
+  @Field(() => [Post], { nullable: true })
+  posts?: [Post] | null;
+
+  @HideField()
+  password: string;
+}
